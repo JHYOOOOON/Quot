a4560f67380d28239436b69013d5e5708dafa040
JHYOOOOON <jhyooooooon@gmail.com>
2023-09-25 17:31:17 +0900

    feat: Form, Card 컴포넌트 띄우기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { Form, Card } from "./components";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Form />
+      <Card />
+    </main>
+  );
 }
